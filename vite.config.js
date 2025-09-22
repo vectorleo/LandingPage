d5d846437f094482aa41b0a08e7e47ae108b4ac8
vectorleo <onwukavictorleo@gmail.com>
2025-09-23 00:09:12 +0100

    kk

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,6 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(),tailwindcss()],
-  base: process/env.VITE_BASE_PATH || "/LandingPage",
+  base: process.env.VITE_BASE_PATH || "/LandingPage",
 
 })
